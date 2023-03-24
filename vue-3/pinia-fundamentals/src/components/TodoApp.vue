96c60d28f210a932d4b87e713935c407851c5748
danielbarrosdeoliveira <danielbarrosdeoliveira@outlook.com>
2023-03-24 17:45:20 -0300

    Lesson: Creating our firststore

--- vue-3/pinia-fundamentals/src/components/TodoApp.vue
+++ vue-3/pinia-fundamentals/src/components/TodoApp.vue
@@ -1,7 +1,15 @@
 <script setup>
+import TodoForm from './TodoForm.vue';
+import TodoList from './TodoList.vue';
+
 </script>
 
 <template>
+  <main class="todo-app">
+    <h1>To Do List</h1>
+    <TodoForm />
+    <TodoList />
+  </main>
 </template>
 
 <style>
