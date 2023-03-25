3415d5d8c2b8e0b29506a367f91f316033ce69ec
danielbarrosdeoliveira <danielbarrosdeoliveira@outlook.com>
2023-03-24 21:52:40 -0300

    Lesson 2 - Mutating state with actions

--- vue-3/pinia-fundamentals/src/components/TodoForm.vue
+++ vue-3/pinia-fundamentals/src/components/TodoForm.vue
@@ -1,7 +1,24 @@
 <script setup>
+import { ref } from 'vue'
+import { useTodoListStore } from '../stores/todoList';
+
+const store = useTodoListStore();
+
+function addItemAndClear(item) {
+  if (item.length === 0) {
+    return
+  }
+  store.addTodo(item);
+  todo.value = ''
+}
+
+const todo = ref('')
 </script>
 
 <template>
+  <form @submit.prevent="addItemAndClear">
+    <input v-model="todo" type="text" /><button>Add</button>
+  </form>
 </template>
 
 <style>
