a6c3c2e620838489986461cb0f8222aab5526730
danielbarrosdeoliveira <danielbarrosdeoliveira@outlook.com>
2023-03-24 22:10:12 -0300

    Lesson 4 - Reactive Properties in Pinia

--- vue-3/pinia-fundamentals/src/components/TodoForm.vue
+++ vue-3/pinia-fundamentals/src/components/TodoForm.vue
@@ -4,6 +4,8 @@ import { useTodoListStore } from '../stores/todoList';
 
 const store = useTodoListStore();
 
+const todo = ref('')
+
 function addItemAndClear(item) {
   if (item.length === 0) {
     return
@@ -12,11 +14,10 @@ function addItemAndClear(item) {
   todo.value = ''
 }
 
-const todo = ref('')
 </script>
 
 <template>
-  <form @submit.prevent="addItemAndClear">
+  <form @submit.prevent="addItemAndClear(todo)">
     <input v-model="todo" type="text" /><button>Add</button>
   </form>
 </template>
